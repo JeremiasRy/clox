bb99b0351c3b107b65ca74455a6650644d083ea5
Jeremias Ryttäri <ryttari.jeremias@gmail.com>
2024-10-09 18:12:40 +0300

    hash table start

--- chunk.c
+++ chunk.c
@@ -36,7 +36,7 @@ void writeChunk(Chunk *chunk, uint8_t byte, int line)
 void writeConstant(Chunk *chunk, Value value, int line)
 {
     int operand = addConstant(chunk, value);
-    if (operand > 255)
+    if (operand >= 1 << 8)
     {
         writeChunk(chunk, OP_CONSTANT_LONG, line);
         writeChunk(chunk, operand & 0xFF, line);
